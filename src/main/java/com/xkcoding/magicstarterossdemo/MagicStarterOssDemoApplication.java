c3be61e710dfbf85b0f6ad3260ef161fe5b737c9
Yangkai.Shen <237497819@qq.com>
2020-01-07 10:17:01 +0800

    :heavy_plus_sign: 添加 magic-starter-oss 依赖

---
+++ src/main/java/com/xkcoding/magicstarterossdemo/MagicStarterOssDemoApplication.java
@@ -0,0 +1,21 @@
+package com.xkcoding.magicstarterossdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2020/1/3 17:08
+ */
+@SpringBootApplication
+public class MagicStarterOssDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicStarterOssDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xkcoding/magicstarterossdemo/MagicStarterOssDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xkcoding.magicstarterossdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MagicStarterOssDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
